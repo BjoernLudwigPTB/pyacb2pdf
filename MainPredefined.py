8465c8401d0e5df36b47c543151a451811480374
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-11-24 10:40:18 +0100

    Adapt test_Initializer and MainPredefined to black

--- MainPredefined.py
+++ MainPredefined.py
@@ -3,16 +3,16 @@ from Core.Initializer import Initializer
 
 
 def convert():
-    input_folder = 'input/'
-    domain = 'https://alpinclub-berlin.de/kv/'
-    xml_filename = 'regulars.xml'
+    input_folder = "input/"
+    domain = "https://alpinclub-berlin.de/kv/"
+    xml_filename = "regulars.xml"
     xml_path = input_folder + xml_filename
     url = domain + xml_filename
     input_path = input_folder + xml_filename
     properties_filename = "kursdaten_prop.properties"
     properties_path = input_folder + properties_filename
-    output_folder = 'output/'
-    output_filename = 'regulars.pdf'
+    output_folder = "output/"
+    output_filename = "regulars.pdf"
     output_path = output_folder + output_filename
     Downloader(url, xml_path)
     Initializer(input_path, output_path, properties_path)
--- test/test_Initializer.py
+++ test/test_Initializer.py
@@ -13,11 +13,11 @@ def test_initializer_init(teststrings):
 
 def test_initializer():
     input_folder = "test/test_data/"
-    xml_filename = 'regulars.xml'
+    xml_filename = "regulars.xml"
     input_path = input_folder + xml_filename
     properties_filename = "testdata_prop.properties"
     properties_path = input_folder + properties_filename
     output_folder = input_folder
-    output_filename = 'regulars.pdf'
+    output_filename = "regulars.pdf"
     output_path = output_folder + output_filename
     Initializer(input_path, output_path, properties_path)
