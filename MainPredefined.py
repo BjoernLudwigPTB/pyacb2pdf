db11d34844e6b289012855e307815b1cc947902b
Wojciech Kur <wojciech.kur96@gmail.com>
2020-12-10 14:56:49 +0100

    Requirements + inits

--- MainPredefined.py
+++ MainPredefined.py
@@ -3,5 +3,5 @@ from Core.Initializer import *
 
 if __name__ == "__main__":
     init = Initializer()
-    init.build("input/template.xml", "output/mypdf.pdf", "input/template_prop.properties", Signature.AUTO_DATE)
+    init.build("input/template.xml", "output/mypdf.pdf", "input/template.properties", Signature.AUTO_DATE)
     print("\n" + "--------------------------------------DONE--------------------------------------")
